8211845719f2dd457a493dca2ab2e5f9b7de0f5d
John YE <johnye@metareco.com>
2020-03-08 15:19:36 +0800

    format2: [AB]->[A][B]
    Signed-off-by: John YE <johnye@metareco.com>

---
+++ format2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+def entry(wen, shi):
+    ret = []
+    W = ''
+    for c in wen:
+        W += '【' + c + '】'
+        
+    print(W + shi)
+
+    
+def volume(v):
+    print(v)
+
+    
+def count(c):
+    print(c)
+
+
+def fourteen(t):
+    print(t)
+
+
+def Copyright(m):
+    print(m)
+
+
+def section(s):
+    print(s)
+    
